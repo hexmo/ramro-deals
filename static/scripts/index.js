50c2e1af2e15a78a449f4130f05e3f28290792b0
hexmo <hitmo3205@gmail.com>
2020-06-29 17:33:13 +0545

    Added javascript function that lets users only to press search button if search input is filled properly

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,15 +1,17 @@
-// Height variables
+// listener
+window.addEventListener('DOMContentLoaded', (event) => {
+    setHeight();
+    typeWriter();
+    searchButton.disabled = true;
+});
+
+
+// Windows sizing system
 var navHeight;
 var footerHeight;
 var windowHeight;
 var searchHeight;
 
-// listener
-window.addEventListener('DOMContentLoaded', (event) => {
-    setHeight();
-});
-
-// calculate and set height
 function setHeight() {
     navHeight = document.getElementById('top-id').offsetHeight;
     footerHeight = document.getElementById('footer1').offsetHeight;
@@ -17,3 +19,28 @@ function setHeight() {
     searchHeight = windowHeight - navHeight - footerHeight;
     document.getElementById("search").style.height = searchHeight + "px";
 }
+
+// Typewriter effect
+var i = 0;
+var txt = 'Search for products. Eg. iPhone 11';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("search-input").placeholder += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+// Enable and disable search button
+var searchBar = document.getElementById("search-input");
+var searchButton = document.getElementById("search-button");
+
+searchBar.addEventListener("keyup", function name(params) {
+    if (searchBar.value.trim().length > 0) {
+        searchButton.disabled = false;
+    } else {
+        searchButton.disabled = true;
+    }
+});
